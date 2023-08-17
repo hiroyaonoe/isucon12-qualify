1db7fe1d00c0b2b5d3e283bbf6a212d258a0893f
Hiroya Onoe <git@onoe.dev>
2023-08-18 02:15:33 +0900

    fix terraform aws version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,4 +15,11 @@ terraform {
     key    = "terraform.tfstate"
     region = "ap-northeast-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
