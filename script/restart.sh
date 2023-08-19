325a5d5b9aba2d4cb2cee760a2fccb2afea5f857
Hiroya Onoe <git@onoe.dev>
2023-08-19 18:36:23 +0900

    fix restart

--- script/restart.sh
+++ script/restart.sh
@@ -4,6 +4,7 @@
 
 # cd ~/webapp/go
 # go build
+sudo systemctl daemon-reload
 sudo systemctl restart isuports.service
 sudo systemctl restart nginx.service # reloadでも良い
 sudo systemctl restart mysql.service
