06d3e576686398b1dd25286bfcf2bc5fe0806213
Hiroya Onoe <git@onoe.dev>
2023-08-19 18:07:08 +0900

    fix install-go

--- script/install-go.sh
+++ script/install-go.sh
@@ -5,7 +5,7 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 rm go1.21.0.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 source ~/.bashrc
 
