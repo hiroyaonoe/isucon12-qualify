4c7319dbc9b161ed8f29d9ef53aeb80b8ce6d048
Hiroya Onoe <git@onoe.dev>
2023-08-19 16:53:26 +0900

    fix script

--- script/install.sh
+++ script/install.sh
@@ -4,6 +4,7 @@
 wget https://github.com/tkuchiki/alp/releases/download/v1.0.14/alp_linux_amd64.tar.gz
 tar -zxvf alp_linux_amd64.tar.gz
 sudo install ./alp /usr/local/bin/alp
+rm alp alp_linux_amd64.tar.gz
 
 # pt-query-digest
 sudo apt-get update
