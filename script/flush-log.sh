cc7f815585f5885ad25e5be91406f48edfbdb3bd
Hiroya Onoe <git@onoe.dev>
2023-08-19 21:41:36 +0900

    fix

--- script/flush-log.sh
+++ script/flush-log.sh
@@ -3,4 +3,4 @@
 echo "" | sudo tee /var/log/nginx/access.log > /dev/null
 
 sudo rm /var/log/mysql/slow.log
-mysqladmin -p flush-logs -ppassword # TODO: password
+mysqladmin -u root -proot flush-logs
