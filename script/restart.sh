d8ca1bd52d5bf2a695e654a64b3f13142dd06b4c
Hiroya Onoe <git@onoe.dev>
2023-08-20 17:33:02 +0900

    fix restart

--- script/restart.sh
+++ script/restart.sh
@@ -2,8 +2,8 @@
 
 # TODO: サービス名、パスは適宜変更
 
-# cd ~/webapp/go
-# go build
+cd ~/webapp/go
+make build
 sudo systemctl daemon-reload
 sudo systemctl restart isuports.service
 sudo systemctl restart nginx.service # reloadでも良い
