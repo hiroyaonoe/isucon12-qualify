1b1340ddb104098ca5b2f955cdb672516242bf78
Hiroya Onoe <git@onoe.dev>
2023-08-20 18:53:25 +0900

    fix pprof

--- script/pprof.sh
+++ script/pprof.sh
@@ -3,6 +3,12 @@
 # TODO: 以下埋める
 URL=localhost:6060 #localの6060にport-forwardした場合
 SOURCE_PATH=~/src/github.com/hiroyaonoe/isucon12-qualify/webapp/go/isuports # localでもbuildしておく
+SECONDS=$1
+
+if [ -z $SECONDS ]; then
+  echo "Please set SECONDS(120くらい?)"
+  exit 1
+fi
 
 # ローカルから実行することを想定 (http://localhost:1080で見れる)
-go tool pprof -http=localhost:1080 ${SOURCE_PATH} http://${URL}/debug/pprof/profile?seconds=60
+go tool pprof -http=localhost:1080 ${SOURCE_PATH} http://${URL}/debug/pprof/profile?seconds=${SECONDS}
